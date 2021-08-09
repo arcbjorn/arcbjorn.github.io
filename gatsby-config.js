0b38dd8024ee5950dd919ce955b5d733ade71731
arcbjorn <arky.happ@gmail.com>
2021-08-09 15:08:23 +0300

    build: add Typescript & css-module-loader

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,6 +8,7 @@ module.exports = {
     "gatsby-plugin-react-helmet",
     "gatsby-plugin-sitemap",
     "gatsby-plugin-postcss",
+    "gatsby-plugin-dts-css-modules",
     {
       resolve: "gatsby-plugin-manifest",
       options: {
