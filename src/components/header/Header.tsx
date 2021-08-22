ec9411c90b8fd6388d315ec899caf249a31b1ea7
arcbjorn <arky.happ@gmail.com>
2021-08-22 15:39:56 +0300

    ref (Header): clean-up

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,23 +1,36 @@
 import React from "react";
 import { Link } from "gatsby";
+import { navigate } from "@reach/router";
 import { ThemeToggler } from "gatsby-plugin-dark-mode";
 
 import ThemeToggle from "components/theme_toggle/ThemeToggle";
 import * as css from "./header.module.css";
 
-export const Header: React.FunctionComponent = () => (
-  <div className={css.header}>
-    <Link className={css.link} to="/">
-      arcbjorn
-    </Link>
-    <Link className={css.link} to="#contact">
-      contact
-    </Link>
-    <Link className={css.link} to="/library">
-      library
-    </Link>
-    <ThemeToggler>{ThemeToggle}</ThemeToggler>
-  </div>
-);
+export const Header: React.FunctionComponent = () => {
+  const route = {
+    home: "/",
+    links: "/#links",
+    library: "/library",
+  };
+
+  const goToLinks = function () {
+    navigate(route.links);
+  };
+
+  return (
+    <div className={css.header}>
+      <Link className="cursor-pointer" to={route.home}>
+        arcbjorn
+      </Link>
+      <div className={css.customLink} onClick={goToLinks}>
+        links
+      </div>
+      <Link className={css.link} to={route.library}>
+        library
+      </Link>
+      <ThemeToggler>{ThemeToggle}</ThemeToggler>
+    </div>
+  );
+};
 
 export default Header;
