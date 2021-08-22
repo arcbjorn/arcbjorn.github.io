89a7fb7a1b994291e7feb2dc7d15ce71c7c88450
arcbjorn <arky.happ@gmail.com>
2021-08-22 15:41:19 +0300

    feat (pages): add base Library page

---
+++ src/pages/library.tsx
@@ -0,0 +1,14 @@
+import * as React from "react";
+
+import Layout from "layouts/Layout";
+
+const Library: React.FunctionComponent = () => {
+  return (
+    <Layout>
+      <title>Library</title>
+      <h1>Books that I like</h1>
+    </Layout>
+  );
+};
+
+export default Library;
