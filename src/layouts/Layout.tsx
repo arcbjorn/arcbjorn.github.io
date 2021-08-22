b7084215b2abbbff864de9fe140bb479bfdf8800
arcbjorn <arky.happ@gmail.com>
2021-08-22 18:13:50 +0300

    feat (Layout): add styles

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 // import * as css from "./layout.module.css";
 import Header from "components/header/Header";
+import * as css from "./layout.module.css";
 
 export const Layout: React.FunctionComponent = ({ children }) => (
-  <div>
+  <div className={css.layout}>
     <Header />
     {children}
   </div>
