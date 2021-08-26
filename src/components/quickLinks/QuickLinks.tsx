97cf2ae232485e6fd15fa407daf31fd5b42fb6d0
arcbjorn <arky.happ@gmail.com>
2021-08-26 10:55:13 +0300

    feat (components): add QuickLinks & styles

---
+++ src/components/quickLinks/QuickLinks.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import * as css from "./quickLinks.module.css";
+
+export const QuickLinks: React.FunctionComponent = () => {
+  return <main className={css.quickLinks}>Links</main>;
+};
+
+export default QuickLinks;
