9c578eb9ae75cf5baadbc3bed97c248cbf129b0a
arcbjorn <arky.happ@gmail.com>
2021-08-27 14:33:32 +0300

    chore (i18n): add dependencies

--- gatsby-config.js
+++ gatsby-config.js
@@ -41,5 +41,31 @@ module.exports = {
         state: path.join(__dirname, "src/state"),
       },
     },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/locales`,
+        name: `locale`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-react-i18next`,
+      options: {
+        localeJsonSourceName: `locale`, // name given to `gatsby-source-filesystem` plugin.
+        languages: [`en`, `ru`, `de`, `se`],
+        defaultLanguage: `en`,
+        // if you are using Helmet, you must include siteUrl, and make sure you add http:https
+        siteUrl: `http://localhost:8000/`,
+        // you can pass any i18next options
+        // pass following options to allow message content as a key
+        i18nextOptions: {
+          interpolation: {
+            escapeValue: false, // not needed for react as it escapes by default
+          },
+          keySeparator: false,
+          nsSeparator: false,
+        },
+      },
+    },
   ],
 };
