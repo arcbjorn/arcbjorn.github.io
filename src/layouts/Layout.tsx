8b149be72070b85e0de7c060779289de277dc144
arcbjorn <arky.happ@gmail.com>
2021-08-29 01:38:03 +0300

    ref (Layout): clean-up

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -1,6 +1,7 @@
 import React from "react";
-// import * as css from "./layout.module.css";
+
 import Header from "components/header/Header";
+
 import * as css from "./layout.module.css";
 
 export const Layout: React.FunctionComponent = ({ children }) => (
