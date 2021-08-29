a5d54c4a69e67c09c2388068a722c35819397804
arcbjorn <arky.happ@gmail.com>
2021-08-29 23:24:50 +0300

    ref (IndePage): move icon library init to Layout

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -1,8 +1,17 @@
 import React from "react";
 
+import { library } from "@fortawesome/fontawesome-svg-core";
+import {
+  faLinkedin,
+  faGithub,
+  faTelegramPlane,
+} from "@fortawesome/free-brands-svg-icons";
+
 import Header from "components/header/Header";
 
 import * as css from "./layout.module.css";
+
+library.add(faLinkedin, faGithub, faTelegramPlane);
 
 export const Layout: React.FunctionComponent = ({ children }) => (
   <div className={css.layout}>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,20 +2,11 @@ import * as React from "react";
 import { Helmet } from "react-helmet";
 import { graphql } from "gatsby";
 
-import { library } from "@fortawesome/fontawesome-svg-core";
-import {
-  faLinkedin,
-  faGithub,
-  faTelegramPlane,
-} from "@fortawesome/free-brands-svg-icons";
-
 import "../styles/tailwind.css";
 import { EDocumentTitle } from "./types";
 
 import Layout from "layouts/Layout";
 import Terminal from "components/terminal/Terminal";
-
-library.add(faLinkedin, faGithub, faTelegramPlane);
 
 const IndexPage: React.FunctionComponent = () => {
   return (
