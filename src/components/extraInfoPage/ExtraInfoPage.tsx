a6a6d104067300f2e205203a5d60a87418cb2472
arcbjorn <arky.happ@gmail.com>
2021-08-29 18:38:17 +0300

    feat (QuickLink): update CV link styles

---
+++ src/components/extraInfoPage/ExtraInfoPage.tsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+
+import * as css from "./extraInfoPage.module.css";
+
+const ExtraInfoPage: React.FunctionComponent = () => {
+  return <main className={css.extraInfoPage}>hello</main>;
+};
+
+export default ExtraInfoPage;
