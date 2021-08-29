c878816e794c3c5ba47efa4fcf5fb5a75239347b
arcbjorn <arky.happ@gmail.com>
2021-08-29 23:28:47 +0300

    feat (Books): add styles & extraPageBorderColor

--- src/components/books/Books.tsx
+++ src/components/books/Books.tsx
@@ -3,13 +3,14 @@ import { Trans } from "gatsby-plugin-react-i18next";
 
 import { ETranslationKey } from "i18n/types";
 import * as css from "./books.module.css";
+import { extraSectionTitle } from "components/extraInfo/extraInfo.module.css";
 
 const Books: React.FunctionComponent = () => {
   return (
     <div className={css.books}>
-      <h1>
+      <div className={extraSectionTitle}>
         <Trans>{ETranslationKey.BOOKS_TITLE}</Trans>
-      </h1>
+      </div>
     </div>
   );
 };
