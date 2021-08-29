71a195deac858953653f067478f618f3dd4f6afe
arcbjorn <arky.happ@gmail.com>
2021-08-29 21:37:34 +0300

    ref (ExtraInfo): fix naming

--- src/components/extraInfo/ExtraInfo.tsx
+++ src/components/extraInfo/ExtraInfo.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 
-import * as css from "./extraInfoPage.module.css";
+import * as css from "./extraInfo.module.css";
 
 const ExtraInfoPage: React.FunctionComponent = () => {
   return <main className={css.extraInfoPage}>hello</main>;
