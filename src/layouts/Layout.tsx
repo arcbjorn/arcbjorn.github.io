5ed8ef293792756229371c1e656398b718ce646a
arcbjorn <arky.happ@gmail.com>
2021-08-30 00:55:57 +0300

    feat (Layout): add header height logic

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { ReactNodeArray } from "react";
 
 import { library } from "@fortawesome/fontawesome-svg-core";
 import {
@@ -13,13 +13,33 @@ import * as css from "./layout.module.css";
 
 library.add(faLinkedin, faGithub, faTelegramPlane);
 
-export const Layout: React.FunctionComponent = ({ children }) => (
-  <div className={css.layout}>
-    <div className="h-1/6">
-      <Header />
+type customNode = {
+  type?: {
+    name: string;
+  };
+};
+
+export const Layout: React.FunctionComponent = ({ children }) => {
+  let headerHeightClass = "h-1/6";
+
+  if ((children as ReactNodeArray).length) {
+    if (
+      (children as ReactNodeArray).some(
+        (node) => (node as customNode).type?.name === "ExtraInfo"
+      )
+    ) {
+      headerHeightClass = "h-1/10";
+    }
+  }
+
+  return (
+    <div className={css.layout}>
+      <div className={headerHeightClass}>
+        <Header />
+      </div>
+      <div className="h-5/6">{children}</div>
     </div>
-    <div className="h-5/6">{children}</div>
-  </div>
-);
+  );
+};
 
 export default Layout;
