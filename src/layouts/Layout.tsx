1c5eeae201284db52876cecd735e66f32bab415d
arcbjorn <arky.happ@gmail.com>
2021-09-01 23:05:50 +0300

    ref (Layout): clean-up

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -1,4 +1,4 @@
-import React, { ReactNodeArray } from "react";
+import React from "react";
 
 import { library } from "@fortawesome/fontawesome-svg-core";
 import {
@@ -13,7 +13,6 @@ import {
 } from "@fortawesome/free-brands-svg-icons";
 
 import Header from "components/header/Header";
-import { isExtraInfoPage } from "./helpers";
 
 import * as css from "./layout.module.css";
 
@@ -29,15 +28,9 @@ library.add(
 );
 
 export const Layout: React.FunctionComponent = ({ children }) => {
-  const headerHeightClass = isExtraInfoPage(children as ReactNodeArray)
-    ? "h-1/10"
-    : "h-1/6";
-
-  isExtraInfoPage;
-
   return (
     <div className={css.layout}>
-      <div className={headerHeightClass}>
+      <div className="h-1/10">
         <Header />
       </div>
       <div className="h-5/6">{children}</div>
