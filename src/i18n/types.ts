ff4f1e7e9e213b44647f5ffddade97436d1bcb82
arcbjorn <arky.happ@gmail.com>
2022-05-01 17:22:51 +0400

    feat (i18n): add Medium description translaton

--- src/i18n/types.ts
+++ src/i18n/types.ts
@@ -18,9 +18,10 @@ export enum Ei18nToken {
   CODEPEN = "extra.platforms.descriptions.codepen",
   HACKER_RANK = "extra.platforms.descriptions.hackerRank",
   LEETCODE = "extra.platforms.descriptions.leetCode",
-  STEPIK = "extra.platforms.descriptions.stepik",
   FREE_CODE_CAMP = "extra.platforms.descriptions.freeCodeCamp",
   DEV_TO = "extra.platforms.descriptions.devto",
+  MEDIUM = "extra.platforms.descriptions.medium",
+  STEPIK = "extra.platforms.descriptions.stepik",
   GLITCH = "extra.platforms.descriptions.glitch",
   MY_TRAVEL_MAP = "extra.platforms.descriptions.myTravelMap",
 
